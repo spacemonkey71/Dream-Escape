3e429aad46c759778928a1e0f3a13a57f4dd2fab
spacemonkey71 <37596002+spacemonkey71@users.noreply.github.com>
2018-03-20 13:03:20 -0700

    Create Dream-Escape.py

---
+++ Dream-Escape.py
@@ -0,0 +1,7 @@
+def start():
+	print """
+	You went to bed in your own bedroom in your own bed. But when you wake up you find yourself on the cold hard floor of a strange room.
+	The room has two doors. One of the doors is a stout wooden door. The other is made of iron.  If you open the wooden door type 'A'.
+	If you open the iron door, type 'B'.
+
+start()
